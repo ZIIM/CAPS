4bdb181372df99ff4d9e01b65ce606544897ec3e
ZIIM <johntgause12@gmail.com>
2024-04-16 10:06:13 -0700

    fixed orderID

--- vendor/handler.js
+++ vendor/handler.js
@@ -5,7 +5,7 @@ const chance = new Chance();
 function newPackage(payload){
   return(payload ={
     store: chance.word({length: 5}),
-    orderID: chance.google_analytics(),
+    orderID: chance.natural(),
     customer: chance.name(),
     address: chance.address(),
   });
