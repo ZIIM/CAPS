9b3cbaa49981bce59ea341da1c418a750d9635f1
ZIIM <johntgause12@gmail.com>
2024-04-17 13:39:12 -0700

    QL

--- clients/driver/handler.js
+++ clients/driver/handler.js
@@ -1,20 +1,20 @@
 function transit(socket, payload) {
   setTimeout(() => {
     console.log('DRIVER', 'picked up', payload);
-  }, 2000);
+  }, 1000);
 
   setTimeout(() => {
     socket.emit('inTransit', payload);
-  }, 3000);
+  }, 2000);
 }
 
 function delivered(socket, payload) {
   setTimeout(() => {
     console.log('DRIVER', 'delivered', payload);
-  }, 2000);
+  }, 1000);
   setTimeout(() => {
     socket.emit('delivered', payload);
-  }, 3000);
+  }, 2000);
 }
 
 module.exports = { transit, delivered };
